21e2020bee354649795c2c7e1be97542dfcd171d
Leandro <leogiova4@gmial.com>
2021-04-30 11:35:59 -0300

    Creando usuarios desde @Bean

--- App1/src/main/java/MiProyecto/App1/Usuario.java
+++ App1/src/main/java/MiProyecto/App1/Usuario.java
@@ -1,6 +1,11 @@
 package MiProyecto.App1;
 
+import java.beans.Transient;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -11,13 +16,24 @@ import lombok.Data;
 public class Usuario{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @Column(name = "username", unique = true)
     private String login;
 
+    @Column(name =  "Nombre")
     private String nombre;
 
+    @Column(name = "Apellido")
     private String apellido;
+
+    @javax.persistence.Transient
+    //no lo muestra a nivel de usuario
+    private String calculo;
 
 
 }
+
+//para insertar en la base de datos
+//INSERT INTO USUARIO (id, username, nombre, apellido) values (2, 'Leandrogiova', 'Leo', 'Giova')
--- App1/src/main/java/MiProyecto/App1/UsuarioRepository.java
+++ App1/src/main/java/MiProyecto/App1/UsuarioRepository.java
@@ -16,25 +16,3 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
  
 }
-
-
-    /* 
-    public List<Usuario> findAll(){
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario u1 = new Usuario();
-        u1.setLogin("LDiamand");
-        u1.setNombre("Luciano");
-        u1.setApellido("Diamand");
-    
-        Usuario u2 = new Usuario();
-        u2.setLogin("Leandrogiova");
-        u2.setNombre("Leandro");
-        u2.setApellido("Giovacchiini");
-
-        usuarios.add(u1);
-        usuarios.add(u2);
-
-        return usuarios;
-    }
-    */
